68666e9497a793e7517df9d5095a5f93480b0c94
Naresh Gurung <gurungnaresh495@gmail.com>
2020-10-10 12:14:34 -0500

    First message

---
+++ src/main/java/com/personal/project/fambam/FambamApplication.java
@@ -0,0 +1,13 @@
+package com.personal.project.fambam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FambamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FambamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/project/fambam/FambamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.project.fambam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FambamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
